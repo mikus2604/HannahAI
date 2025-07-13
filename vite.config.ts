9cca2701d3884d0867843bf96e5636bab0460d82
Lenovo <mikus2604@gmail.com>
2025-07-13 16:13:47 +0100

    modified:   package-lock.json 	modified:   package.json 	deleted:    postcss.config.js 	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  plugins: [react()],
   server: {
-    host: "::",
-    port: 8080,
+    open: true, // Automatically open browser
+    host: '0.0.0.0', // Listen on all network interfaces
+    port: 8080
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+});
